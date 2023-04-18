3299882ea09c215d149d74f8ae94510feb9cd5cf
Aaron <admin@Aarons-MacBook-Pro.local>
2023-04-18 14:42:08 +0700

    create custom prebuild script

---
+++ prebuild.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const env = process.env.NODE_ENV || 'development';
+const source = './.swcrc';
+
+/**
+ * SWC compiler don't have capabilities to load different env config file
+ * so we need to do this hacky way so we could load different swc config depends
+ * on the environment
+ */
+fs.readFile(source, 'utf-8', (err, files) => {
+  if (err) {
+    console.error("Read file err: ", err);
+    return err;
+  }
+
+  console.log("ENV: ", env);
+  if (env === 'development') {
+    return;
+  }
+
+  const swcrcConfig = JSON.parse(files);
+  swcrcConfig.minify = true;
+  swcrcConfig.jsc.minify = {
+    compress: {
+      unused: true
+    },
+    mangle: true
+  }
+  
+  const json = JSON.stringify(swcrcConfig, null, 2);
+  fs.writeFile('./.swcrcNonDev', json, 'utf-8', (err) => {
+    if (err) {
+      console.error("Write file err: ",err)
+    }
+  });
+});
